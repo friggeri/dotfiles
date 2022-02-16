58f7ca73905af857e300543b0f86a5a647faa32d
Adrien Friggeri <adrien@friggeri.net>
2022-02-15 19:55:41 -0700

    Correct path..

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes
 
+dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 mkdir -p $HOME/.config
-ln -s $HOME/.dotfiles/starship.toml $HOME/.config/starship.toml
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
+ln -s $dir/starship.toml $HOME/.config/starship.toml
+ln -s $dir/.zshrc $HOME/.zshrc
+ln -s $dir/.gitconfig $HOME/.gitconfig
