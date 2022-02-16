0a39afaeb4546be0f84d8e63ddac44d1e0921727
Adrien Friggeri <adrien@friggeri.net>
2022-02-15 20:07:35 -0700

    Force delete files

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes
 
 dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 mkdir -p $HOME/.config
-rm $HOME/.config/starship.toml && ln -s $dir/starship.toml $HOME/.config/starship.toml
-rm $HOME/.zshrc && ln -s $dir/.zshrc $HOME/.zshrc
-rm $HOME/.gitconfig && ln -s $dir/.gitconfig $HOME/.gitconfig
+rm -f $HOME/.config/starship.toml && ln -s $dir/starship.toml $HOME/.config/starship.toml
+rm -f $HOME/.zshrc && ln -s $dir/.zshrc $HOME/.zshrc
+rm -f $HOME/.gitconfig && ln -s $dir/.gitconfig $HOME/.gitconfig
